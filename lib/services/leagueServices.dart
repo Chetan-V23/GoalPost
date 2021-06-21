e9edc3c4cf72501dc4aa8ddbc797428f6800a1d4
Chetan Vallish <chetanvallish@gmail.com>
2021-06-21 22:04:09 +0530

    Added Cross contry leagues and cups

--- lib/services/leagueServices.dart
+++ lib/services/leagueServices.dart
@@ -1,45 +1,37 @@
-
 import 'baseAPIService.dart';
 
 class LeagueService {
   static Future<List<dynamic>> getLeagues() async {
-
     List<dynamic> filteredList = [];
     var response = await BaseAPIHelper.get('/v3/leagues');
-    bool spain=false, england=false, italy=false, france=false, germany=false;
+    bool spain = false,
+        england = false,
+        italy = false,
+        france = false,
+        germany = false;
     print("\n\nGot response");
-    for(var single in response)
-    {
-      
-      String name=single['country']['name'].toString().toLowerCase();
-      if(spain==false&&name=='spain')
-      {
-        spain=true;
-;
+    for (var single in response) {
+      String name = single['country']['name'].toString().toLowerCase();
+      if (spain == false && name == 'spain') {
+        spain = true;
+        ;
+        filteredList.add(single['league']);
+      } else if (england == false && name == 'england') {
+        england = true;
+        filteredList.add(single['league']);
+      } else if (germany == false && name == 'germany') {
+        germany = true;
+        filteredList.add(single['league']);
+      } else if (france == false && name == 'france') {
+        france = true;
+        filteredList.add(single['league']);
+      } else if (italy == false && name == 'italy') {
+        italy = true;
+        filteredList.add(single['league']);
+      } else if (single['league']['id'] >=1&& single['league']['id']<=10) {
         filteredList.add(single['league']);
       }
-      else if(england==false&&name=='england')
-      {
-        england=true;
-        filteredList.add(single['league']);
-      }
-      else if(germany==false&&name=='germany')
-      {
-        germany=true;
-        filteredList.add(single['league']);
-      }
-      else if(france==false&&name=='france')
-      {
-        france=true;
-        filteredList.add(single['league']);
-      }
-      else if(italy==false&&name=='italy')
-      {
-        italy=true;
-        filteredList.add(single['league']);
-      }
-      if(france&&spain&&germany&&italy&&england)
-      {
+      if (france && spain && germany && italy && england) {
         break;
       }
     }
